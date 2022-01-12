ca0e09866e6ed1b4f7a43e53b0896e9e52f0c291
Akrat Chandola <akratchandola@gmail.com>
2022-01-12 20:13:50 +0530

    Update R_3.cc

--- R_3.cc
+++ R_3.cc
@@ -5,18 +5,8 @@ void fibo(int n,int i)
 	static int a=1,b=1,c;
 	if(i>n)
 		return;
-	if(n==1)
-	{
-		cout<<"1";
-		return;
-	}
-	if(n==2)
-	{
-		cout<<"1 1";
-		return;
-	}
 	c=a+b;
-	cout<<c<<" ";
+	cout<<" + "<<c;
 	a=b;
 	b=c;
 	return fibo(n,i+1);
@@ -26,6 +16,17 @@ int main()
 { 
     int n,i=1;
 	cin>>n;
-	fibo(n,i);
+	if(n==1)
+	{
+		cout<<"1";
+		return 0;
+	}
+	if(n==2)
+	{
+		cout<<"1 + 1";
+		return 0;
+	}
+	cout<<"1 + 1";
+	fibo(n,i+2);
     return 0; 
 } 
