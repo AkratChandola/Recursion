41caeb11ab2e58fbfcea7eb657ad2fcd570d1bc9
Akrat Chandola <akratchandola@gmail.com>
2022-01-12 18:51:35 +0530

    Update R_1d.cc

--- R_1d.cc
+++ R_1d.cc
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-void natural(int n,int i)
+void odd(int n,int i)
 {
 	if(i>n)
 		return;
 	cout<<i<<" ";
-	return natural(n,i+2);
+	return odd(n,i+2);
 }
 
 int main() 
 { 
     int n,i=1;
 	cin>>n;
-    natural(n,i);
+    odd(n,i);
     return 0; 
 } 
